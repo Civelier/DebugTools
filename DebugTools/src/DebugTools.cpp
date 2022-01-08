9d423200981fad6a1351bfc04742087074c2f58a
Civelier Algon <civelier.algon@gmail.com>
2022-01-08 18:36:43 -0500

    Add project files.

---
+++ DebugTools/src/DebugTools.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		DebugTools.cpp
+ Created:	1/8/2022 6:36:10 PM
+ Author:	civel
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "DebugTools.h"
+
+
---
+++ DebugTools/src/DebugTools.h
@@ -0,0 +1,19 @@
+/*
+ Name:		DebugTools.h
+ Created:	1/8/2022 6:36:10 PM
+ Author:	civel
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _DebugTools_h
+#define _DebugTools_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
